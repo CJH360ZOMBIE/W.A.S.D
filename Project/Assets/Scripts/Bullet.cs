953e5062fdac6b154be2f5751a44194d5f29d57c
CJH360ZOMBIE <charliejhargreaves@gmail.com>
2020-09-23 10:19:20 +0100

    1

---
+++ Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bullet;
+    // public GameObject hitEffect;
+
+    void start()
+    {
+
+    }
+ void OnBecameInvisible() //destroys bullet when out of screen bounds
+{
+        Destroy (bullet);
+}
+}
